29766262401a6454e5f8b08e49d0f3850ea36072
Mineev <mineev.buryat@gmail.com>
2017-06-16 23:55:36 +0800

    реализован правильный алгоритм поиска корней

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -2,10 +2,15 @@ from math import sqrt
 
 
 def get_roots(a, b, c):
+    #найти дискриминант
     discriminant = b ** 2 - 4 * a * c
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
+    #исходя из значения дискриминанта найти корни
     if discriminant == 0:
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
         return root1, None
-    else:
+    elif discriminant > 0:
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
+        root2 = (-b + sqrt(discriminant)) / (2 * a)
         return root1, root2
+    else:
+        return None, None
